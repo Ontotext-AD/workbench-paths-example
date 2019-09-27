7ab402367e85c1ffc760df8f82c3a117d65bcd82
desislava <desislava.hristova@ontotext.com>
2019-09-27 17:53:50 +0300

    Implement find path function to find all paths between two nodes

--- src/js/angular/graphexplore/controllers/paths.controller.js
+++ src/js/angular/graphexplore/controllers/paths.controller.js
@@ -38,6 +38,51 @@ define([
                 initForRepository();
             });
             initForRepository();
+
+            var maxPathLength = 3;
+
+            var findPath = function (startNode, endNode, visited, path) {
+                // A path is found, return a promise that resolves to it
+                if (startNode === endNode) {
+                    return $q.when(path)
+                }
+                // Find only paths with maxLength, we want to cut only short paths between airports
+                if (path.length === maxPathLength) {
+                    return $q.when([])
+                }
+                return $http({
+                    url: 'rest/explore-graph/links',
+                    method: 'GET',
+                    params: {
+                        iri: startNode,
+                        config: 'default',
+                        linksLimit: 50
+                    }
+                }).then(function (response) {
+                    // Use only links with the hasFlightTo predicate
+                    var flights = _.filter(response.data, function(r) {return r.predicates[0] == "hasFlightTo"});
+                    // For each links, continue to search path recursively
+                    var promises = _.map(flights, function (link) {
+                        var o = link.target;
+                        if (!visited.includes(o)) {
+                            return findPath(o, endNode, visited.concat(o), path.concat(link));
+                        }
+                        return $q.when([]);
+                    });
+                    // Group together all promises that resolve to paths
+                    return $q.all(promises);
+                }, function (response) {
+                    var msg = getError(response.data);
+                    toastr.error(msg, 'Error looking for path node');
+                });
+            }
+
+            $scope.findPath = function (startNode, endNode) {
+                findPath(startNode, endNode, [startNode], []).then(function (linksFound) {
+                    console.log(_.flattenDeep(linksFound));
+                    //renderGraph(_.flattenDeep(linksFound));
+                });
+            }
         }
     }
 );
